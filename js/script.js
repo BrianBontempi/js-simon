b87aeb518ea78dbec311d4defc4b12caf31cca3c
BrianBontempi <brian.bont54@gmail.com>
2024-01-17 18:44:54 +0100

    add timer function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// ! recupero l'elemento in pagina
+const numbersElement = document.getElementById('numbers')
+
 // Funzione per generare 5 numeri casuali
 function generateRandomNumbers() {
     let numbers = [];
@@ -8,3 +11,15 @@ function generateRandomNumbers() {
   }
   
   
+// Funzione per visualizzare i numeri e avviare il timer
+function displayNumbers() {
+    let numbers = generateRandomNumbers();
+    numbersElement.innerText = numbers.join(' ');
+  
+    // Avvia il timer di 10 secondi
+    setTimeout(function() {
+      numbersElement.innerText = '';
+      getUserInput(numbers);
+    }, 10000);
+  }
+
